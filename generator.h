d8b99978c1321e7cb4424bf8450f95746ef9b2f6
TogExe <66035138+TogExe@users.noreply.github.com>
2025-04-05 17:45:12 +0200

    Update generator.h

--- generator.h
+++ generator.h
@@ -5,6 +5,15 @@
 #ifndef GENERATOR_H
 #define GENERATOR_H
 
+#define MAX_OPTIONS 4
+
+typedef struct {
+    int options[MAX_OPTIONS];
+    int option_count;
+    int collapsed; // 1 if a tile has been chosen
+} Cell;
+
+char* build_amap_please_i_want_one_very_very_much(int size);
 
 
 #endif //GENERATOR_H
