da708f35a51f49f17b5fd0a48b2498e717be9580
TogExe <66035138+TogExe@users.noreply.github.com>
2025-04-22 16:07:36 +0200

    Update generator.h
    updating the header

--- generator.h
+++ generator.h
@@ -5,15 +5,10 @@
 #define GENERATOR_H
 
 typedef struct {
-    int opt_count;
-    char *** opt;
-    char ** choice;
+    int status;
+    char opt[13];
+    char choice;
 } tile;
 
-#define TILE_SIZE 3
-#define MAP_SIZE 10
 
-void initialize_map(tile map[MAP_SIZE][MAP_SIZE], int size);
-void free_map(tile map[MAP_SIZE][MAP_SIZE], int size);
-void generation(tile map[MAP_SIZE][MAP_SIZE], int size);
 #endif // GENERATOR_H
