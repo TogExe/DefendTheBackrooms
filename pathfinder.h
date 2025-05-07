743784bbfa5f8e0b19da44f708175d41987865bb
TogExe <66035138+TogExe@users.noreply.github.com>
2025-05-07 12:52:43 +0200

    Update pathfinder.h

--- pathfinder.h
+++ pathfinder.h
@@ -1,6 +1,6 @@
 #ifndef PATHFINDER_H
 #define PATHFINDER_H
 
-int generate_organic_path(char grid[MAX_SIDE][MAX_SIDE], int side, char *avoid, int avoid_count, char *prefer, int prefer_count);
+int generate_organic_path(char grid[MAX_SIDE][MAX_SIDE], int side);
 
 #endif
