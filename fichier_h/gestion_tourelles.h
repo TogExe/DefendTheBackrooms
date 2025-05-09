61742e795fe22d686a93341fddf6845f30b2ed3b
TogExe <66035138+TogExe@users.noreply.github.com>
2025-05-09 14:11:46 +0200

    Add files via upload

--- fichier_h/gestion_tourelles.h
+++ fichier_h/gestion_tourelles.h
@@ -5,6 +5,8 @@
 
 int draw_click_zone(SDL_Renderer *renderer, int mouse_x, int mouse_y,int side,  char map[side][side], int tile_size,
                     int *out_x, int *out_y, int click_positions[MAX_CLICKS][2], int click_count, int argent);
+//int draw_click_zone(SDL_Renderer *renderer, int mouse_x, int mouse_y,int side, char ** map, int tile_size,
+//                    int *out_x, int *out_y, int click_positions[MAX_CLICKS][2], int click_count, int argent);
 void turret(Tower* towers, int tower_count, Enemy* enemies, int enemy_count, Mix_Chunk *tir_son, int tile_size);
 
 #endif
